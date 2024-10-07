96ad529d2592f7d750d92e9ca58be126c6f8e348
NaimaZoulaikhi <zoulnaima2002@gmail.com>
2024-10-07 17:10:55 +0100

    @Zoul

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 export const AppConfig = () => ({
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/e-commerce',
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce',
   port: parseInt(process.env.PORT, 10) || 3000,
   baseUrl: process.env.BASE_URL || 'http://localhost',
   apiPrefix: process.env.API_PREFIX || 'api/v1',
